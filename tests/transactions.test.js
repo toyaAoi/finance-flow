f32e1fd58143cf340820cbbd974a17e2b056f0e0
lho-lt <164295110+lho-lt@users.noreply.github.com>
2024-08-14 22:52:23 +0530

    #7 replaced it with todo

--- tests/transactions.test.js
+++ tests/transactions.test.js
@@ -1,9 +1,9 @@
-const { describe, it } = require("node:test");
+const { describe, todo } = require("node:test");
 
 describe("Transactions", () => {
-  it("should be able to create a transaction");
-  it("should be able to get all transaction");
-  it("should be able to get a transaction by id");
-  it("should be able to delete a transaction");
-  it("should be able to update a transaction");
+  todo("should be able to create a transaction");
+  todo("should be able to get all transaction");
+  todo("should be able to get a transaction by id");
+  todo("should be able to delete a transaction");
+  todo("should be able to update a transaction");
 });
