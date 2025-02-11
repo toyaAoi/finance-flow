18b24b597ff864a39803213672b21054490f3460
lho <164295110+lho-lt@users.noreply.github.com>
2025-02-11 10:55:39 +0530

    🔧 Fixed mongodb connection error

--- server/src/database/connectDB.js
+++ server/src/database/connectDB.js
@@ -5,9 +5,12 @@ import logger from "../utils/logger.utils.js";
 const connectDB = async () => {
   try {
     logger.info("Connecting to MongoDB");
+    if (config.NODE_ENV !== "production") {
+      mongoose.set("debug", true);
+    }
     mongoose.set("strictQuery", false);
     if (!config.MONGODB_URI) throw new Error("MONGODB_URI is not defined");
-    mongoose.connect(config.MONGODB_URI);
+    await mongoose.connect(config.MONGODB_URI);
     logger.info("Connected to MongoDB");
   } catch (error) {
     logger.error("Error connecting to MongoDB: ", error.message);
