09ce5f3323840a8d3750deb98f90ca9e28639074
toyaAoi <96932308+toyaAoi@users.noreply.github.com>
2025-02-19 21:14:29 +0530

    🪄 Refactored

--- web-ui/src/components/CategoryChart.tsx
+++ web-ui/src/components/CategoryChart.tsx
@@ -1,5 +1,4 @@
-import { Legend, Pie, PieChart } from "recharts";
-
+import { Pie, PieChart, Legend } from "recharts";
 import {
 	Card,
 	CardContent,
@@ -27,36 +26,25 @@ export function CategoryChart() {
 	} = useAccountStore();
 
 	if (!expenseThisMonthByCategory) {
-		return;
+		return null;
 	}
 
-	const chartConfig = {} as ChartConfig;
-	const chartData = [] as Category[];
-
-	for (const category in expenseThisMonthByCategory) {
-		chartConfig[category] = { label: category };
-
-		const categoryData = expenseThisMonthByCategory[category];
-
-		chartData.push({
-			name: category,
-			amount: categoryData.amount,
-			fill: categoryData.color.toLowerCase(),
-		});
-	}
+	const { chartConfig, chartData } = prepareChartData(
+		expenseThisMonthByCategory,
+	);
 
 	return (
-		<Card className="flex flex-col">
+		<Card className="row-span-2 flex flex-col">
 			<CardHeader className="pb-0">
 				<CardTitle>Spending this month</CardTitle>
 				<CardDescription>
 					{new Date().toLocaleString(undefined, { month: "long" })}
 				</CardDescription>
 			</CardHeader>
-			<CardContent className="flex-1 pb-0">
+			<CardContent className="flex-1 pb-2">
 				<ChartContainer
 					config={chartConfig}
-					className="mx-auto aspect-square max-h-[300px] mb-4"
+					className="mx-auto aspect-square w-full h-full max-h-[300px] mb-4"
 				>
 					<PieChart>
 						<ChartTooltip
@@ -64,10 +52,36 @@ export function CategoryChart() {
 							content={<ChartTooltipContent hideLabel />}
 						/>
 						<Pie data={chartData} dataKey="amount" nameKey="name" />
-						<Legend />
+						<Legend
+							layout="vertical"
+							verticalAlign="middle"
+							align="right"
+							wrapperStyle={{ paddingLeft: "20px" }}
+						/>
 					</PieChart>
 				</ChartContainer>
 			</CardContent>
 		</Card>
 	);
+}
+
+function prepareChartData(
+	expenseData: Record<string, { amount: number; color: string }>,
+) {
+	const chartConfig: ChartConfig = {};
+	const chartData: Category[] = [];
+
+	for (const category in expenseData) {
+		chartConfig[category] = { label: category };
+
+		const { amount, color } = expenseData[category];
+
+		chartData.push({
+			name: category,
+			amount,
+			fill: color.toLowerCase(),
+		});
+	}
+
+	return { chartConfig, chartData };
 }
