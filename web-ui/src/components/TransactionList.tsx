b2ca2e69f200d4ce620c4587d38bddc29f517278
toyaAoi <96932308+toyaAoi@users.noreply.github.com>
2025-02-19 20:49:13 +0530

    🪄 Refactored

--- web-ui/src/components/TransactionList.tsx
+++ web-ui/src/components/TransactionList.tsx
@@ -7,43 +7,40 @@ export function TransactionList({ data }: { data?: Transaction[] }) {
     account: { recentTransactions },
   } = useAccountStore();
 
-  if (!recentTransactions) {
-    return;
-  }
+  const transactions = data || recentTransactions;
 
-  const transactions = data ? data : recentTransactions;
+  if (!transactions) {
+    return null;  }
 
   return (
-    <Card>
-      <CardHeader>
+    <Card className='col-start-1 col-span-2 row-span-3'>
+      <CardHeader className="pt-6 pb-3">
         <CardTitle>Recent Transactions</CardTitle>
       </CardHeader>
-      <CardContent>
+      <CardContent className="h-[calc(100%-3rem)] overflow-y-scroll">
         <div className="space-y-4">
           {transactions.map((transaction) => (
-            <div
-              key={transaction.id}
-              className="flex items-center justify-between p-4 border rounded-lg"
-            >
-              <div className="space-y-1">
-                <p className="font-medium">{transaction.notes}</p>
-                <p className="text-sm text-muted-foreground">
-                  {transaction.category.name}
-                </p>
-              </div>
-              <p
-                className={
-                  transaction.type === "income"
-                    ? "text-primary-foreground"
-                    : "text-accent"
-                }
-              >
-                {transaction.type === "income" ? "+" : "-"}${transaction.amount}
-              </p>
-            </div>
+            <TransactionItem key={transaction.id} transaction={transaction} />
           ))}
         </div>
       </CardContent>
     </Card>
   );
 }
+
+const TransactionItem = ({ transaction }: { transaction: Transaction }) => {
+  return (
+    <div className="flex items-center justify-between p-4 border rounded-lg">
+      <div className="space-y-1">
+        <p className="font-medium">{transaction.notes}</p>
+        <p className="text-sm text-muted-foreground">
+          {transaction.category.name}
+        </p>
+      </div>
+      <p className={transaction.type === "income" ? "text-primary-foreground" : "text-accent"}>
+        {transaction.type === "income" ? "+" : "-"}${transaction.amount}
+      </p>
+    </div>
+  );
+};
+
